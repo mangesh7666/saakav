63bdf72250a63d10c094bdb5a191b63d89061388
mangesh7666 <mangeshkuswah1210@gmail.com>
2026-01-20 17:21:21 +0530

    Update cancelled-crops.js

--- frontend/src/admin_components/cancelled-crops.js
+++ frontend/src/admin_components/cancelled-crops.js
@@ -13,7 +13,7 @@ export default function AdminCancelledCrops() {
     try {
       setLoading(true);
       const res = await axios.get(
-        "http://localhost:5000/api/admin/crops/rejected",
+        "https://saakav1.onrender.com/api/admin/crops/rejected",
         { headers: { Authorization: `Bearer ${token}` } }
       );
       setCrops(res.data);
