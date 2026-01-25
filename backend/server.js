2a46c62133e08c47946a7425cd533d87dd68eacb
mangesh7666 <mangeshkuswah1210@gmail.com>
2026-01-25 18:14:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 /* ================= MIDDLEWARE ================= */
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));
+app.use(
+  "/uploads",
+  express.static(path.join(__dirname, "uploads"))
+);
 /* ================= DATABASE ================= */
 mongoose
   .connect(process.env.MONGO_URI)
